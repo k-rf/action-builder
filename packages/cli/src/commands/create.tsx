77d2ecf52167b4679bf68a5002d800d3b513283d
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-12-23 15:56:16 +0000

    ♻️ #1 Refactor create command to use CreateForm component

--- packages/cli/src/commands/create.tsx
+++ packages/cli/src/commands/create.tsx
@@ -1,16 +1,12 @@
 import { Spinner } from "@inkjs/ui";
-import { Box, Spacer, Text, useApp, useFocusManager } from "ink";
+import { Text, useApp, useFocusManager } from "ink";
 import React, { useEffect, useState } from "react";
 
-import { createFiles } from "../app/create-files.js";
-import { Confirm } from "../components/Confirm.js";
-import { Field } from "../components/Field.js";
+import { CreateForm } from "../components/CreateForm.js";
 import { useCursorMove } from "../hooks/use-cursor-move.js";
 import { getDotGitPath } from "../io/get-dot-git-path.js";
 
 export default function Create() {
-  const [actionName, setActionName] = useState("");
-  const [actionDescription, setActionDescription] = useState("");
   const [dotGitPath, setDotGitPath] = useState<string | undefined>(undefined);
 
   const [errorMessage, setErrorMessage] = useState<string | undefined>(
@@ -18,16 +14,12 @@ export default function Create() {
   );
 
   const { exit } = useApp();
-  const { focus, focusNext, focusPrevious } = useFocusManager();
+  const { focusNext, focusPrevious } = useFocusManager();
 
   useCursorMove({
     onUp: focusPrevious,
     onDown: focusNext,
   });
-
-  useEffect(() => {
-    focus("1");
-  }, [dotGitPath]);
 
   useEffect(() => {
     getDotGitPath()
@@ -48,48 +40,5 @@ export default function Create() {
     return <Spinner />;
   }
 
-  // TODO: 別コンポーネントとして切り出す。
-  return (
-    <Box flexDirection="column" width={80}>
-      <Box
-        height={5}
-        borderStyle={"bold"}
-        borderColor={"yellowBright"}
-        alignItems="center"
-      >
-        <Spacer />
-        <Text>Create a new custom action.</Text>
-        <Spacer />
-      </Box>
-      <Field
-        id="1"
-        label="Action name"
-        placeholder="Enter action name..."
-        value={actionName}
-        onChange={setActionName}
-        onSubmit={focusNext}
-      />
-      <Field
-        id="2"
-        label="Action description"
-        placeholder="Enter action description..."
-        value={actionDescription}
-        onChange={setActionDescription}
-        onSubmit={focusNext}
-      />
-      <Confirm
-        id="3"
-        onConfirm={async () => {
-          await createFiles({
-            dotGitPath: dotGitPath,
-            name: actionName,
-            description: actionDescription,
-          });
-
-          exit();
-        }}
-        onCancel={exit}
-      />
-    </Box>
-  );
+  return <CreateForm dotGitPath={dotGitPath} />;
 }
---
+++ packages/cli/src/components/CreateForm.tsx
@@ -0,0 +1,66 @@
+import { Box, Spacer, Text, useApp, useFocusManager } from "ink";
+import React, { useEffect, useState } from "react";
+
+import { createFiles } from "../app/create-files.js";
+import { Confirm } from "../components/Confirm.js";
+import { Field } from "../components/Field.js";
+
+type CreateFormProps = {
+  dotGitPath: string;
+};
+
+export const CreateForm = ({ dotGitPath }: CreateFormProps) => {
+  const [actionName, setActionName] = useState("");
+  const [actionDescription, setActionDescription] = useState("");
+
+  const { exit } = useApp();
+  const { focus, focusNext } = useFocusManager();
+
+  useEffect(() => {
+    focus("1");
+  }, []);
+
+  return (
+    <Box flexDirection="column" width={80}>
+      <Box
+        height={5}
+        borderStyle={"bold"}
+        borderColor={"yellowBright"}
+        alignItems="center"
+      >
+        <Spacer />
+        <Text>Create a new custom action.</Text>
+        <Spacer />
+      </Box>
+      <Field
+        id="1"
+        label="Action name"
+        placeholder="Enter action name..."
+        value={actionName}
+        onChange={setActionName}
+        onSubmit={focusNext}
+      />
+      <Field
+        id="2"
+        label="Action description"
+        placeholder="Enter action description..."
+        value={actionDescription}
+        onChange={setActionDescription}
+        onSubmit={focusNext}
+      />
+      <Confirm
+        id="3"
+        onConfirm={async () => {
+          await createFiles({
+            dotGitPath: dotGitPath,
+            name: actionName,
+            description: actionDescription,
+          });
+
+          exit();
+        }}
+        onCancel={exit}
+      />
+    </Box>
+  );
+};
